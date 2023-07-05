f6d08f62c3610066275a4ab8b1fcd8abb82c2a81
Naresh kumar <nareshxo@Nareshs-Air.wind3.hub>
2023-07-05 13:24:44 +0200

    final commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# AngularRedditClone
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.4.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Redit clone
+
+- this is a spring boot application with spring mvc, spring security with JwT authentication, spring data JPA with mysql, and angular 9.
+- The user can login and create a post or upvote or downvote the existing vote or he can be able to create subreddit and post and comment the posts.
+- for the mail i will be using the fake mail server, mail trap. and all the mails that our app will be sending to this mail box.
+
+# Dependencies
+
+- spring web
+- lombok
+- spring security
+- spring data JPA
+- Mysql java driver
+- java mail sender
+
+- 
---
+++ src/app/token-interceptor.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, BehaviorSubject, throwError } from 'rxjs';
+import { AuthService } from './auth/shared/auth.service';
+import { catchError, switchMap, take, filter } from 'rxjs/operators';
+import { LoginResponse } from './auth/login/login-response.payload';
+
+@Injectable({
+    providedIn: 'root'
+})
+// this interceptor will retrive the auth token from the local storage
+export class TokenInterceptor implements HttpInterceptor {
+
+    isTokenRefreshing = false;
+    refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject(null); // the behaviorsubject will act as the semaphore to block all the outgoing calls, the behavior subject can ve a val assigned to it, so when we receive the new tokenn from the refresh token() we can assigne the val to this behavior subject, and access the new token inside the interceptor
+
+    constructor(public authService: AuthService) { }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler):
+        Observable<HttpEvent<any>> {
+
+        if (req.url.indexOf('refresh') !== -1 || req.url.indexOf('login') !== -1) {
+            return next.handle(req);
+        }
+        const jwtToken = this.authService.getJwtToken();
+
+        if (jwtToken) {
+            return next.handle(this.addToken(req, jwtToken)).pipe(catchError(error => {
+                if (error instanceof HttpErrorResponse
+                    && error.status === 403) {
+                    return this.handleAuthErrors(req, next);
+                } else {
+                    return throwError(error);
+                }
+            }));
+        }
+        return next.handle(req);
+
+    }
+// if  the token is invalid then we ve to block all the outgoing calls for this user
+    private handleAuthErrors(req: HttpRequest<any>, next: HttpHandler)
+        : Observable<HttpEvent<any>> {
+        if (!this.isTokenRefreshing) {
+            this.isTokenRefreshing = true;
+            this.refreshTokenSubject.next(null);
+
+            return this.authService.refreshToken().pipe(
+                switchMap((refreshTokenResponse: LoginResponse) => {
+                    this.isTokenRefreshing = false;
+                    this.refreshTokenSubject
+                        .next(refreshTokenResponse.authenticationToken);
+                    return next.handle(this.addToken(req,
+                        refreshTokenResponse.authenticationToken));
+                })
+            )
+        } else {
+            return this.refreshTokenSubject.pipe(
+                filter(result => result !== null),
+                take(1),
+                switchMap((res) => {
+                    return next.handle(this.addToken(req,
+                        this.authService.getJwtToken()))
+                })
+            );
+        }
+    }
+
+    addToken(req: HttpRequest<any>, jwtToken: any) {
+        return req.clone({
+            headers: req.headers.set('Authorization',
+                'Bearer ' + jwtToken)
+        });
+    }
+
+}
